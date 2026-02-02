1367a6d1f0f751a60978e8da2a7511de84624e5c
zhazken-maryam <zhazkenasem@gmail.com>
2026-02-02 11:20:01 +0500

    my commit

--- lab2/Task6.py
+++ lab2/Task6.py
@@ -1,8 +1,8 @@
 n = int(input())
 arr = list(map(int, input().split()))
 
-max = arr[0]
+max_el = arr[0]
 for i in range(1, len(arr)):
-    if arr[i] > max:
-        max = arr[i]
-print(max)
+    if arr[i] > max_el:
+        max_el = arr[i]
+print(max_el)

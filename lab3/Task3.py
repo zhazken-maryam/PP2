8f8ded86ee47ed6ba1c7ab7e93ff54239dc81356
zhazken-maryam <zhazkenasem@gmail.com>
2026-02-16 15:39:34 +0500

    my commit

--- lab3/Task3.py
+++ lab3/Task3.py
@@ -1,13 +1,13 @@
-# convert letters to digits
+# dictionary: letters -> digit
 d = {
     "ZER":"0","ONE":"1","TWO":"2","THR":"3","FOU":"4",
     "FIV":"5","SIX":"6","SEV":"7","EIG":"8","NIN":"9"
 }
 
-# reverse dictionary
+# dictionary: digit -> letters
 r = {v:k for k,v in d.items()}
 
-s = input()
+s = input()   # get input
 
 # find operator
 if "+" in s:
@@ -17,21 +17,33 @@ elif "-" in s:
 else:
     op = "*"
 
-a, b = s.split(op)
+a, b = s.split(op)   # split into two numbers
 
-# function to convert
-def to_num(x):
-    res = ""
+
+# convert letters to integer
+def to_int(x):
+    num = ""
     for i in range(0, len(x), 3):
-        res += d[x[i:i+3]]
-    return int(res)
+        num += d[x[i:i+3]]
+    return int(num)
 
-# calculate
-res = eval(str(to_num(a)) + op + str(to_num(b)))
 
-# convert back
-out = ""
+n1 = to_int(a)   # first number
+n2 = to_int(b)   # second number
+
+
+# calculate result
+if op == "+":
+    res = n1 + n2
+elif op == "-":
+    res = n1 - n2
+else:
+    res = n1 * n2
+
+
+# convert result back to letters
+ans = ""
 for digit in str(res):
-    out += r[digit]
+    ans += r[digit]
 
-print(out)
+print(ans)   # print answer
